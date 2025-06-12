b703c1217b8a6ac5b605606ec221adf9583fe739
nwiedel <164350832+nwiedel@users.noreply.github.com>
2025-06-12 08:15:15 +0200

    2. ViewportUtils Klasse

--- core/src/main/java/com/avoid/screen/GameScreen.java
+++ core/src/main/java/com/avoid/screen/GameScreen.java
@@ -1,38 +1,49 @@
 package com.avoid.screen;
 
+import com.avoid.config.GameConfig;
+import com.avoid.util.Utilities;
+import com.avoid.util.ViewportUtils;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen implements Screen {
 
-    private SpriteBatch batch;
-    private Texture image;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private ShapeRenderer renderer;
 
     @Override
     public void show() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(GameConfig.WORLD_WIDTH, GameConfig.WORLD_HEIGHT, camera);
+        renderer = new ShapeRenderer();
     }
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 70, 210);
-        batch.end();
+        ScreenUtils.clear(Color.BLACK);
+
+        drawDebug();
+    }
+
+    private void drawDebug(){
+        ViewportUtils.drawGrid(viewport, renderer);
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        renderer.dispose();
     }
 
     @Override
     public void resize(int width, int height) {
-
+        viewport.update(width, height);
+        ViewportUtils.debugPixelsPerUnit(viewport);
     }
 
     @Override
