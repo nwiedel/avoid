5f2a53d4dde9e38d22c9c282b5046024e24286cd
nwiedel <164350832+nwiedel@users.noreply.github.com>
2025-06-13 22:43:55 +0200

    3.1. Player Bewegungn

--- core/src/main/java/com/avoid/entity/Player.java
+++ core/src/main/java/com/avoid/entity/Player.java
@@ -1,5 +1,7 @@
 package com.avoid.entity;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 
@@ -7,6 +9,7 @@ public class Player {
 
     private static final float BOUNDS_RADIUS = 0.4f;
     private static final float SIZE = 2 * BOUNDS_RADIUS;
+    private static final float MAX_X_SPEED = 0.25f;
 
     private float x;
     private float y;
@@ -27,7 +30,28 @@ public class Player {
         updateBounds();
     }
 
+    public void update(){
+        float xSpeed = 0f;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)){
+            xSpeed = MAX_X_SPEED;
+        }else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            xSpeed = -MAX_X_SPEED;
+        }
+
+        x += xSpeed;
+        updateBounds();
+    }
+
     private void updateBounds(){
         bounds.setPosition(x, y);
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
     }
 }
--- core/src/main/java/com/avoid/screen/GameScreen.java
+++ core/src/main/java/com/avoid/screen/GameScreen.java
@@ -8,11 +8,14 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class GameScreen implements Screen {
+
+    private static final Logger log = new Logger(GameScreen.class.getSimpleName(), Logger.DEBUG);
 
     private OrthographicCamera camera;
     private Viewport viewport;
@@ -34,9 +37,21 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        // update world
+        update(delta);
+
         ScreenUtils.clear(Color.BLACK);
 
         renderDebug();
+    }
+
+    private void update(float delta){
+        updatePlayer();
+    }
+
+    private void updatePlayer(){
+        //log.debug("playerX = " + player.getX() + " playerY = " + player.getY());
+        player.update();
     }
 
     private void renderDebug(){
