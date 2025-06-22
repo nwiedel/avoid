af05baa88da397a652eda5339c515f4a338150f1
NicolasWiedel <nicolaswiedel@googlemail.com>
2025-06-22 17:23:13 +0200

    7. Base Klasse für Gameobjekte erstellt

--- core/src/main/java/com/avoid/entity/Obstacle.java
+++ core/src/main/java/com/avoid/entity/Obstacle.java
@@ -5,46 +5,19 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 
-public class Obstacle {
+public class Obstacle extends GameObjectBase {
 
     private static final float BOUNDS_RADIUS = 0.3f;
     private static final float SIZE = 2 * BOUNDS_RADIUS;
 
-    private float x;
-    private float y;
-
     private float ySpeed = 0.1f;
 
-    private Circle bounds;
-
     public Obstacle(){
-        bounds = new Circle(x, y, BOUNDS_RADIUS);
-    }
-
-    public void drawDebug(ShapeRenderer renderer){
-        renderer.circle(bounds.x, bounds.y, bounds.radius, 30);
-    }
-
-    public void setPosition(float x, float y){
-        this.x = x;
-        this.y = y;
-        updateBounds();
+        super(BOUNDS_RADIUS);
     }
 
     public void update(){
-        setPosition(x, y - ySpeed);
-    }
-
-    private void updateBounds(){
-        bounds.setPosition(x, y);
-    }
-
-    public float getX() {
-        return x;
-    }
-
-    public float getY() {
-        return y;
+        setY(getY() - ySpeed);
     }
 
     public float getWidth() {
